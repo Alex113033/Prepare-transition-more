8ecf1a632c22983cf35284189f7394754fcd066d
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-10-19 23:41:48 +0300

    Initial Commit

---
+++ Prepare transition more/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prepare transition more
+//
+//  Created by Александр on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
